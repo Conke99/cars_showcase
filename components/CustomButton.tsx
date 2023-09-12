1395788188dd8b73cc34b75a8228a880bb7db11e
Aleksandar Conic <acaconic99@gmail.com>
2023-09-12 18:08:18 +0200

    Creation of Navbar & Footer, applyed prettier

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,21 +1,24 @@
-"use client"
+"use client";
 
-import { CustomButtonProps } from "@/types"
-import Image from "next/image"
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
 
-const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+}: CustomButtonProps) => {
   return (
-    <button 
-    disabled={false}
-    type={"button"}
-    className={`custom-btn ${containerStyles}`}
-    onClick={handleClick}
+    <button
+      disabled={false}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
     >
-        <span className={`fles-1`}>
-            {title}
-        </span>
+      <span className={`fles-1`}>{title}</span>
     </button>
-  )
-}
+  );
+};
 
-export default CustomButton
+export default CustomButton;
