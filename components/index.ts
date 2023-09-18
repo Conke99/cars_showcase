33f31c5f7c58099d8cd00ac740cbe7e2fc3604a0
Aleksandar Conic <acaconic99@gmail.com>
2023-09-18 17:57:54 +0200

    show more button added

--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,12 @@
 import Hero from "./Hero"
 import CustomButton from "./CustomButton"
 import Footer from "./Footer"
-import NavBar from "./NavBar"
+import NavBar from "./Navbar"
 import SearchBar from "./SearchBar"
 import CustomFilter from "./CustomFilter"
 import SearchManufacturer from "./SearchManufacturer"
 import CarCard from "./CarCard"
 import CarDetails from "./CarDetails"
+import ShowMore from "./ShowMore"
 
-export {Hero, CustomButton, NavBar, Footer, CustomFilter, SearchBar, SearchManufacturer, CarDetails, CarCard}
+export {Hero, CustomButton, NavBar, Footer, CustomFilter, SearchBar, SearchManufacturer, CarDetails, CarCard, ShowMore}
--- types/index.ts
+++ types/index.ts
@@ -52,3 +52,8 @@ export interface SearchManuFacturerProps {
     manufacturer: string;
     setManuFacturer: (manufacturer: string) => void;
   }
+
+  export interface ShowMoreProps {
+    pageNumber: number;
+    isNext: boolean
+  }
